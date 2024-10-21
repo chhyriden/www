47ad5d53bf5f7a9e216e69e335dfe12b64a4d1e6
Chhy Riden <33048444+chhyriden@users.noreply.github.com>
2024-10-21 17:19:58 +0700

    wow

---
+++ web3/wp-content/jetpack-waf/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+define( 'DISABLE_JETPACK_WAF', false );
+if ( defined( 'DISABLE_JETPACK_WAF' ) && DISABLE_JETPACK_WAF ) return;
+define( 'JETPACK_WAF_MODE', 'silent' );
+define( 'JETPACK_WAF_SHARE_DATA', false );
+define( 'JETPACK_WAF_SHARE_DEBUG_DATA', false );
+define( 'JETPACK_WAF_DIR', 'C:\\laragon\\www\\web3/wp-content/jetpack-waf' );
+define( 'JETPACK_WAF_WPCONFIG', 'C:\\laragon\\www\\web3/wp-content/../wp-config.php' );
+require_once 'C:\\laragon\\www\\web3\\wp-content\\plugins\\jetpack/vendor/autoload.php';
+Automattic\Jetpack\Waf\Waf_Runner::initialize();
